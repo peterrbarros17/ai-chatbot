51fda4f6980e97c87ef888d2cd9dcc6bcdc84cd0
Peter <34892565+peterrbarros17@users.noreply.github.com>
2024-02-16 16:41:52 -0300

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,12 +1,13 @@
 import { HfInference } from "@huggingface/inference";
 import { HuggingFaceStream, StreamingTextResponse } from "ai";
 import { experimental_buildOpenAssistantPrompt } from "ai/prompts";
+import { NextRequest } from "next/server";
 
 const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
 
 export const runtime = "edge";
 
-export async function POST(req: Request) {
+export async function POST(req: Request | NextRequest) {
   const { messages } = await req.json();
   const response = Hf.textGenerationStream({
     model: "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
